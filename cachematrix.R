21660a9ecd641519720e067b07d135a8b5b8d6e8
Piotr <piotr.p.jagiello@gmail.com>
2014-06-21 09:54:12 -0400

    solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is pretty much line the example, the only real
+## differences being the input to the first function
+## is a matrix, and we're using solve rather than mean
 
-## Write a short comment describing this function
-
+## This is the state function thing.  It uses 'solve'
+## for the actual computation.  The assignment said
+## to assume the matrix was invertible, so we don't have
+## to check for that, and we don't.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse<-NULL
+  
+  set<-function(y){
+    x<<-y
+    inverse<<-NULL
+  }
+  get<-function()x
+  
+  set_inverse<-function(solve)inverse<<-solve
+  get_inverse<-function()inverse
+  
+  list(set=set,get=get,
+       set_inverse=set_inverse,get_inverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'
+## The input into this is the fancy pants list above
+## made with the matrix you want
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse<-x$get_inverse()
+  if (!is.null(inverse)){
+    message("getting cached inverse")
+    return(inverse)
+  }
+  data<-x$get()
+  
+  ## the assignment allowed us to assume an inverse exists
+  inverse<-solve(data,...)
+  x$set_inverse(inverse)
+  inverse
 }
